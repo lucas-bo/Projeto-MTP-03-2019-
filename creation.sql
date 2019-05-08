b0acd4520c0a777e8b737a1aec62f838e0080141
Filipe Verri <filipeverri@gmail.com>
2019-05-08 18:31:29 -0300

    Fixes and organization.

--- creation.sql
+++ creation.sql
@@ -1,76 +1,69 @@
 CREATE TABLE IF NOT EXISTS People (
-  PeopleID    INTEGER PRIMARY KEY AUTOINCREMENT,
-  Name  TEXT UNIQUE,
-  BirthDate date,
-  CPF  int
+  id    INTEGER PRIMARY KEY AUTOINCREMENT,
+  cpf   INTEGER UNIQUE,
+  name  TEXT NOT NULL,
+  birthday INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS Questions (
-  QuestionID    INTEGER PRIMARY KEY AUTOINCREMENT,
-  Statement TEXT NOT NULL,
-  AlternativeType int --Tipo de alternativa: tipo1, tipo2, tipo3 etc
+CREATE TABLE IF NOT EXISTS Question (
+  id        INTEGER PRIMARY KEY AUTOINCREMENT,
+  statement TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Alternatives (
-  AlternativeID    INTEGER PRIMARY KEY AUTOINCREMENT,
-  AlternativeText TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS Alternative (
+  id   INTEGER PRIMARY KEY AUTOINCREMENT,
+  text TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Quizes (
-  QuizID    INTEGER PRIMARY KEY AUTOINCREMENT,
-  Cdate date, --Data de criação.
+CREATE TABLE IF NOT EXISTS Quiz (
+  id    INTEGER PRIMARY KEY AUTOINCREMENT,
+  label TEXT UNIQUE NOT NULL,
 );
-
 
 CREATE TABLE IF NOT EXISTS QuestionQuizComposition (
-  Question INTEGER,
-  Quiz INTEGER,
+  question INTEGER NOT NULL,
+  quiz INTEGER NOT NULL,
 
-  PRIMARY KEY (Question, Quiz),
-  FOREIGN KEY (Questions) REFERENCES Questions(QuestionID) ON DELETE CASCADE
-  FOREIGN KEY (Quiz) REFERENCES Quizes(quizID) ON DELETE CASCADE,
+  PRIMARY KEY (question, quiz),
+  FOREIGN KEY (question) REFERENCES Question(id) ON DELETE CASCADE,
+  FOREIGN KEY (quiz) REFERENCES Quiz(id) ON DELETE CASCADE
 );
-
 
 CREATE TABLE IF NOT EXISTS QuestionAlternativeComposition (
-  Question INTEGER,
-  Alternative INTEGER,
+  question INTEGER NOT NULL,
+  alternative INTEGER NOT NULL,
 
   PRIMARY KEY (question, alternative),
-  FOREIGN KEY (questions) REFERENCES Questions(questionID) ON DELETE CASCADE
-  FOREIGN KEY (alternative) REFERENCES Alternatives (alternativeID) ON DELETE CASCADE,
+  FOREIGN KEY (question) REFERENCES Question(id) ON DELETE CASCADE,
+  FOREIGN KEY (alternative) REFERENCES Alternative (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Answers History(
-  Person INTEGER,
-  Alternative INTEGER,
-  Question INTEGER,
-  Quiz INTEGER,
-  Cdate date, --Data de criação.
-  Answer TEXT,
+CREATE TABLE IF NOT EXISTS AnswersHistory (
+  person INTEGER NOT NULL,
+  alternative INTEGER NOT NULL,
+  question INTEGER NOT NULL,
+  quiz INTEGER NOT NULL,
+  date INTEGER NOT NULL,
 
-  PRIMARY KEY (people, alternative, question, quiz, Cdate),
-  FOREIGN KEY (people) REFERENCES People(peopleID) ON DELETE CASCADE,
-  FOREIGN KEY (alternative) REFERENCES Alternatives(alternativeID) ON DELETE CASCADE,
-  FOREIGN KEY (questions) REFERENCES Questions(questionID) ON DELETE CASCADE
-  FOREIGN KEY (quiz) REFERENCES Quizes(quizID) ON DELETE CASCADE,
+  PRIMARY KEY (person, alternative, question, quiz, date),
+  FOREIGN KEY (person) REFERENCES People(id) ON DELETE CASCADE,
+  FOREIGN KEY (alternative) REFERENCES Alternative(id) ON DELETE CASCADE,
+  FOREIGN KEY (question) REFERENCES Question(id) ON DELETE CASCADE,
+  FOREIGN KEY (quiz) REFERENCES Quiz(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE IF NOT EXISTS Subjects (
-  SubjectID    INTEGER PRIMARY KEY AUTOINCREMENT,
-  Name  TEXT UNIQUE
+CREATE TABLE IF NOT EXISTS Subject (
+  id    INTEGER PRIMARY KEY AUTOINCREMENT,
+  name  TEXT UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS SubjectQuestionComposition (
-  Subject INTEGER,
-  Question INTEGER,
+  subject INTEGER,
+  question INTEGER,
 
   PRIMARY KEY (subject, question),
-  FOREIGN KEY (subject) REFERENCES Subjects(subjectID) ON DELETE CASCADE,
-  FOREIGN KEY (question) REFERENCES Questions(questionID) ON DELETE CASCADE
+  FOREIGN KEY (subject) REFERENCES Subject(id) ON DELETE CASCADE,
+  FOREIGN KEY (question) REFERENCES Question(id) ON DELETE CASCADE
 );
 
-
 PRAGMA foreign_keys = ON;
-
